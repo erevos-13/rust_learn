38cafc926439b22c6c4e385ecd8a118b6c57b155
orpheasboutsarides <erevos13@gmail.com>
2023-01-31 08:27:22 +0200

    create the planet cal

--- mars_cal/src/main.rs
+++ mars_cal/src/main.rs
@@ -1,109 +1,112 @@
+mod scructs;
+use scructs::planet::{GravityOnPlanets, Planet};
 use std::io;
-const GRAVITY: f32 = 9.81;
-#[derive(Debug)]
-enum GravityOnPlanets {
-    MERCURY,
-    MOON,
-    VENUS,
-    MARS,
-    JUPYTER,
-    NEPTUNE,
-    URANUS,
-    SATURN,
-}
-#[derive(Debug)]
-struct Planet {
-    id: i16,
-    name: GravityOnPlanets,
-    gravity: f32,
-}
 
-impl Planet {
-    fn call_weight_on_planet(&self, weight: &f32) -> f32 {
-        (weight / GRAVITY) * self.gravity
-    }
-}
-
-fn get_planet(Planet { name, gravity, id }: Planet) -> i16 {
-    id
-}
 fn main() {
-    let planet_on_moon = Planet {
-        id: 1,
-        gravity: 1.622,
-        name: GravityOnPlanets::MOON,
-    };
-    let planet_on_mercury = Planet {
-        id: 2,
-        gravity: 3.7,
-        name: GravityOnPlanets::MERCURY,
-    };
-    let planet_on_venus = Planet {
-        id: 3,
-        gravity: 8.87,
-        name: GravityOnPlanets::VENUS,
-    };
-    let planet_on_mars = Planet {
-        id: 4,
-        gravity: 3.711,
-        name: GravityOnPlanets::MARS,
-    };
-    let planet_on_jupyter = Planet {
-        id: 5,
-        gravity: 24.79,
-        name: GravityOnPlanets::JUPYTER,
-    };
-    let planet_on_neptune = Planet {
-        id: 6,
-        gravity: 11.15,
-        name: GravityOnPlanets::NEPTUNE,
-    };
-    let planet_on_uranus = Planet {
-        id: 7,
-        gravity: 8.69,
-        name: GravityOnPlanets::URANUS,
-    };
-    let planet_on_saturn = Planet {
-        id: 8,
-        gravity: 10.44,
-        name: GravityOnPlanets::SATURN,
-    };
-    let mut planets: Vec<Planet> = vec![
-        planet_on_mercury,
-        planet_on_moon,
-        planet_on_venus,
-        planet_on_jupyter,
-        planet_on_mars,
-        planet_on_neptune,
-        planet_on_saturn,
-        planet_on_uranus,
-    ];
-    let mut selected_planet = String::new();
-
-    for iter in &planets {
-        println!("Planet id: {:?}, {:?}", iter.id, iter.name);
-    }
+    let planet_moon = Planet::new("Moon".to_string(), 1.662, 1);
+    let planet_mercury = Planet::new("Mercury".to_string(), 3.7, 2);
+    let planet_venus = Planet::new("Venus".to_string(), 8.87, 3);
+    let planet_mars = Planet::new("Mars".to_string(), 3.77, 4);
+    let planet_jupyter = Planet::new("Jupyter".to_string(), 24.79, 5);
+    let planet_neptune = Planet::new("Neptune".to_string(), 11.15, 6);
+    let planet_uranus = Planet::new("Uranus".to_string(), 8.69, 7);
+    let planet_saturn = Planet::new("Saturn".to_string(), 10.44, 8);
     println!("Select the planet?");
-    io::stdin().read_line(&mut selected_planet).unwrap();
-    let planet_id: i16 = selected_planet.trim().parse().unwrap();
-    for iter in planets {
-        if (iter.id == planet_id) {
-            found_plante(iter);
+    println!("1. Mercury");
+    println!("2. Moon");
+    println!("3. Venus");
+    println!("4. Mars");
+    println!("5. Jupyter");
+    println!("6. Neptune");
+    println!("7. Uranus");
+    println!("8. Saturn");
+    println!("10 EXIT the program");
+
+    loop  {
+        let mut selected_planet = String::new();
+        println!("Enter the number of the planet:");
+        io::stdin().read_line(&mut selected_planet).unwrap();
+        let planet_id: i32 = selected_planet.trim().parse().unwrap();
+
+        if planet_id == 10 {
+            println!("You are exiting the program");
             break;
         }
-    }
-}
-fn found_plante(iter: Planet) {
-    let planet_selected = iter;
-    let mut input = String::new();
-    println!("Selected planet: {:?}", planet_selected);
-    println!("Enter your weight on Earth (kg):");
-    io::stdin().read_line(&mut input).unwrap();
-    let weight: f32 = input.trim().parse().unwrap();
+        let mut weight = String::new();
+        println!("Enter your current weight:");
+        io::stdin().read_line(&mut weight).unwrap();
 
-    println!(
-        "Weight on {:?} your weight is: {:?}",
-        planet_selected.name,
-        planet_selected.call_weight_on_planet(&weight)
-    );
+        let weight_number: f32 = weight.trim().parse().unwrap();
+
+        match planet_id {
+            1 => {
+                let w = planet_moon.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_moon.get_name(),
+                    w
+                )
+            },
+            2 => {
+                let w = planet_mercury.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_mercury.get_name(),
+                    w
+                )
+            },
+            3 => {
+                let w = planet_venus.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_venus.get_name(),
+                    w
+                )
+            },
+            4 => {
+                let w = planet_mars.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_mars.get_name(),
+                    w
+                )
+            },
+            5 => {
+                let w = planet_jupyter.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_jupyter.get_name(),
+                    w
+                )
+            },
+            6 => {
+                let w = planet_neptune.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_neptune.get_name(),
+                    w
+                )
+            },
+            7 => {
+                let w = planet_uranus.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_uranus.get_name(),
+                    w
+                )
+            },
+            8 => {
+                let w = planet_saturn.cal_gravity_on_planet(weight_number);
+                println!(
+                    "Yot weight in {} is: {}",
+                    planet_saturn.get_name(),
+                    w
+                )
+            },
+            _ => println!("The planet number you selected is not exist"),
+        }
+    }
+
+
+
 }
